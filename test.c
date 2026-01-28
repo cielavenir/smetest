d24184a3818bd56e045193458e98df495c6e6112
cielavenir <cielartisan@gmail.com>
2026-01-28 12:50:55 +0900

    add void to arg

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 // Accepted
-__attribute__((target("+sme"))) void ok1() __arm_streaming {}
-__attribute__((target("+sme"))) __arm_locally_streaming void ok2() {}
+__attribute__((target("+sme"))) void ok1(void) __arm_streaming {}
+__attribute__((target("+sme"))) __arm_locally_streaming void ok2(void) {}
 
 // Rejected
-__attribute__((target("+sme"))) __arm_streaming void bad1() {}
-__attribute__((target("+sme"))) void bad2() __arm_locally_streaming {}
+__attribute__((target("+sme"))) __arm_streaming void bad1(void) {}
+__attribute__((target("+sme"))) void bad2(void) __arm_locally_streaming {}
