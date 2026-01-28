fc470188758ee00a2ef4bd8ea8307f95757a8a2e
cielavenir <cielartisan@gmail.com>
2026-01-28 12:49:54 +0900

    add attributes

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 // Accepted
-void ok1() __arm_streaming {}
-__arm_locally_streaming void ok2() {}
+__attribute__((target("+sme"))) void ok1() __arm_streaming {}
+__attribute__((target("+sme"))) __arm_locally_streaming void ok2() {}
 
 // Rejected
-__arm_streaming void bad1() {}
-void bad2() __arm_locally_streaming {}
+__attribute__((target("+sme"))) __arm_streaming void bad1() {}
+__attribute__((target("+sme"))) void bad2() __arm_locally_streaming {}
